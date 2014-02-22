bd0ae79eb8bd80786fd505c9a739bbf71b1dea26
Phil Vargas <null>
2014-02-22 13:53:29 -0800

    added unit test

--- sinatra-skeleton-mvc/spec/unit_test_spec.rb
+++ sinatra-skeleton-mvc/spec/unit_test_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe "Unit Tests" do
-  before(:all)
-    FactoryGirl.create(:note)
+describe User do
+  before(:all) do
+    FactoryGirl.create(:card)
     FactoryGirl.create(:deck)
     FactoryGirl.create(:user)
   end
 
-  validate_presence_of(:username).with_message("Username cannot be left blank")
-  validate_presence_of(:password).with_message("Password cannot be left blank")
+  it { should validate_presence_of(:username).with_message("Username cannot be left blank")}
+  it { should validate_presence_of(:password).with_message("Password cannot be left blank")}
 end
 
